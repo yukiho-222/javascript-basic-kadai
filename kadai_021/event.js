7e80c8657c63bf3165facfd87b801c68875ae213
yukiho-222 <kuwatayukiho@gmail.com>
2025-02-12 22:22:38 +0900

    Update event.js

--- kadai_021/event.js
+++ kadai_021/event.js
@@ -1,5 +1,8 @@
-btn.addEventListener('click', () => { 
+const btn = document.getElementById('btn');  // ボタンの要素を取得
+const text = document.getElementById('text'); // h2の要素を取得
+
+btn.addEventListener('click', () => { // クリックイベントを追加
     setTimeout(() => { 
-        text.textContent = 'ボタンをクリックしました';
+        text.textContent = 'ボタンをクリックしました'; // 2秒後にテキストを変更
     }, 2000);
 });
